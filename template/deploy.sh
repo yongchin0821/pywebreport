fb44864acadcc694267581f4510136e642493991
Yongchin <yongchin39@qq.com>
2022-05-27 16:58:50 +0800

    update

--- template/deploy.sh
+++ template/deploy.sh
@@ -1,20 +1,19 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-# 生成dist
+# gen dist
 rm -rf dist
 mkdir dist
 cp index.html dist/
 cp report.css dist/
 cp city.png dist/
 
-# 进入生成的文件夹
+# cd folder
 cd dist
 git init
 git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://SeldomQA.github.io
 git push -f https://github.com/yongchin0821/pywebreport.git main:gh-pages
 
 cd -
